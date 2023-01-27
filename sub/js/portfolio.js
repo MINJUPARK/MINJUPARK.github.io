41ef29209ee2c7ab1b0b800fef45abeea1f76de2
MINJUPARK <psj0943@naver.com>
2023-01-27 12:43:23 +0900

    포트폴리오 이미지 상세, 링크 연결 (미완성)

--- sub/js/portfolio.js
+++ sub/js/portfolio.js
@@ -24,21 +24,20 @@ $(function() {
         },
 
         afterSlideLoad: function(section, origin, destination, direction, trigger) {
-            console.log(section,origin,destination,direction,trigger );
             $(`.monitor__screen img, .phone__screen img`).removeClass('scroll');
 
             if(!destination || destination == 0) {
                 $(".help__comment").fadeOut(function() {
-                    $(this).text("카테고리 이동은 상/하 휠 또는 드래그, 작업물 상세는 좌/우 드래그로 확인할 수 있습니다.").fadeIn(300);
+                    $(this).text("카테고리 변경은 상/하, 작업물 상세는 좌/우로 이동하여 확인할 수 있습니다. (휠, 드래그, 방향키 지원)").fadeIn(300);
                 });
-
             } else {
                 if(section == 'web') {
                     $(`.slide.fp-slide.fp-table.active .monitor__screen img, .slide.fp-slide.fp-table.active .phone__screen img`).addClass('scroll');
-                } else if (section == 'design') {
-                    $(".help__comment").text("이미지 클릭 시 상세보기로 이동합니다.").fadeIn(300);
+                }
+                if(section == 'design') {
+                    $(".help__comment").text("이미지 클릭 시 상세 보기로 이동합니다.").fadeIn(300);
                 } else {
-                    $(".help__comment").text("모니터나 스마트폰 화면 클릭 시 상세보기로 이동합니다.").fadeIn(300);
+                    $(".help__comment").text("모니터나 스마트폰 화면 클릭 시 상세 페이지로 이동합니다.").fadeIn(300);
                 }
             }
         }
@@ -61,5 +60,24 @@ function prepareImg() {
     const list = document.getElementsByClassName("prepare");
     for (let i = 0; i < list.length; ++i) {
         list[i].setAttribute('src', './image/prepare.png');
+    }
+}
+
+
+function originalImg() {
+    const img = document.querySelectorAll('.origin');
+    const currentURL = window.location.href;
+    let setURL;
+
+    for (let p = 0; p < img.length; p++) {
+        img[p].onclick = function() {
+            if(this.dataset.url) {
+                setURL = currentURL.substring(0, currentURL.indexOf('portfolio')) + this.dataset.url;
+            }
+            else {
+                setURL = this.src;
+            }
+            window.open(setURL);
+        }
     }
 }
